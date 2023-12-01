661bc48e948a44c4e7ec03fbdf0ac8859fba2d6b
OhMesch <mze5111@gmail.com>
2023-12-01 14:57:51 -0500

    adds daily template

---
+++ 2023/0_template/ab_test.py
@@ -0,0 +1,10 @@
+import a
+import b
+
+def test_part_a_solution_generation():
+    a_example_solution = 
+    assert a.generateSolution("a_test.dat") == a_example_solution
+
+def test_part_b_solution_generation():
+    b_example_solution = =
+    assert b.generateSolution("b_test.dat") == b_example_solution
